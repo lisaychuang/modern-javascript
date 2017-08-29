b09314ccd26dd86560fdd5e432ffc90d99e7a3c1
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-29 16:22:33 -0700

    Updated solution for exercise 4 - Cashier cart

--- exercises/cashier/src/cashier.js
+++ exercises/cashier/src/cashier.js
@@ -1,4 +1,28 @@
 export function cashier() {
   // TODO: implement me
 
+  let items = [];
+
+  return {
+    add(item){
+      items.push(item);
+      return this;
+    },
+
+    get length(){
+      let sum = 0;
+      for (let i=0; i< items.length; i++){
+        sum = sum + items[i].qty;
+      }
+      return sum;
+    },
+
+    get total(){
+      let tot = 0;
+      for (let i=0; i< items.length; i++){
+        tot = tot + (items[i].price * items[i].qty);
+      }
+      return tot;
+    }
+  }
 }
