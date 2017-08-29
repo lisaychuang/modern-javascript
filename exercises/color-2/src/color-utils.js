bfd72843126b7c17b0f6a73ea0c4a99813ba6530
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-29 14:19:43 -0700

    Updated solution for exercise 2, color modules

--- exercises/color-2/src/color-utils.js
+++ exercises/color-2/src/color-utils.js
@@ -1,2 +1,24 @@
 // Implement your color utils here
 
+export function rgbToHex(r,g,b){
+    return [r, g, b]
+    .map(function boxChannel(ch){
+        return Math.max(0, Math.min(255, ch));
+    })
+    .map(function toHex(ch){
+        return ch.toString(16);
+    })
+    .map(function padding(ch){
+        if (ch.length <2) return `0${ch}`;
+        return ch;
+    })
+    .join ('');
+};
+
+export function hexToRgb(hx) {
+    let hex = hx.length === 3
+        ? `${hx[0]}${hx[0]}${hx[1]}${hx[1]}${hx[2]}${hx[2]}`
+        : hx;
+    let [r, g, b] = [0, 2, 4].map(x => parseInt(hex.substring(x, x + 2), 16));
+    return { r, g, b };
+};
