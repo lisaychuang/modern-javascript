a1b193612f9a6c81594c82faee4cb8f06b2928bb
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-30 11:22:58 -0700

    Updated solution for exercise 6, Versioned Documents

--- exercises/document/src/document.js
+++ exercises/document/src/document.js
@@ -8,6 +8,31 @@ export function Doc(doc = { version: CURRENT_DOC_VERSION, data: {} }) {
 Doc.prototype = {
   _loadDocument(docVersion, data) {
     // TODO: Update document based on version
+
+    let split = (data.name|| ' ').split(' ');
+
+    switch (docVersion) {
+      case 1:
+        data.firstName = split[0]
+        data.lastName = split.length > 1 
+        ? split[split.length -1]
+        : '';
+        data.location = "Earth";
+      case 2:
+        data.email = "nobody@example.com";
+      case 3:
+        if (split.length > 2){
+          data.middleName = split.slice(1, split.length-1).join(' ');
+        } else {
+
+          data.middleName = "";
+        };
+      case 4:
+        break;
+      default:
+        throw `No spec for this version ${docVersion}`;
+    }
+
     this._data = data;
   },
   get data() {
@@ -16,6 +41,15 @@ Doc.prototype = {
 };
 
 Doc.fromFile = function fromFile(filename) {
-  let doc = require(`../docs/${filename}`);
-  return new Doc(doc);
+  try {
+    let doc = require(`../docs/${filename}`);
+    return new Doc(doc);
+  } catch (e) {
+    let {message} = e;
+    if (message && message.indexOf('find') >=0) {
+      throw `Cannot find document: ${filename}`;
+    } else {
+      throw e;
+    }
+  }
 };
