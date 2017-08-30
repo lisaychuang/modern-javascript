e4443fdb2f20c3049fa827359bec3d731182975b
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-30 14:02:11 -0700

    Updated solution for exercise 8, Fetch Plus

--- exercises/fetchplus/src/fetchplus.js
+++ exercises/fetchplus/src/fetchplus.js
@@ -1,6 +1,14 @@
 import { default as _fetch } from 'node-fetch';
 
+// mutable value
+const data = {}
+
 // Do not change this function signature or the tests will break
 export default function fetchPlus(url, options, fetch = _fetch ) {
+    let key = url;
+    if (!data[key]) {
+        data[key] = fetch(key).then(response => response.json());
+    }
+    return data[key]
 
 }
