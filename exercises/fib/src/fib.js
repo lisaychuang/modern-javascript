d33ed58a76a46084ea1008543c7818ed88b7faf0
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-30 13:25:19 -0700

    Updated solution for exercise 7, Fib

--- exercises/fib/src/fib.js
+++ exercises/fib/src/fib.js
@@ -8,5 +8,17 @@
 
 export function getFibSequence(length) {
   // TODO: replace this with your implementation
-  return [];
+  if (typeof length === 'undefined' || length === null 
+    || (typeof length === 'number' && length < 0)) 
+    return undefined;
+    
+  if (length === 0) return [];
+  if (length === 1) return [1];
+
+  let fib = [1,1];
+
+  for (let i =2; i< length; i++){
+    fib.push(fib[i-1] + fib[i-2])
+  }
+  return fib;
 }
